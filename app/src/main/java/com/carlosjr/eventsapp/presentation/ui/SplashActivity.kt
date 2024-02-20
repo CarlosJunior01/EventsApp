6b2f179744adf4fd4a2d68687726016299758b3c
carlos.junior <carlos.udi@live.com>
2024-02-20 00:04:52 -0300

    Organize packages and rename MainActivity to SplashActivity

--- app/src/main/java/com/carlosjr/eventsapp/presentation/ui/SplashActivity.kt
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.carlosjr.eventsapp
+package com.carlosjr.eventsapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.carlosjr.eventsapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
