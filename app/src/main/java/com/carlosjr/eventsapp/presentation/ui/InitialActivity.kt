cf535133727000fadc78566b3a5926c6860c72c8
carlos.junior <carlos.udi@live.com>
2024-02-20 01:06:50 -0300

    Updated InitialActivity added accessibility for events_logo

--- app/src/main/java/com/carlosjr/eventsapp/presentation/ui/InitialActivity.kt
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/InitialActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.carlosjr.eventsapp.databinding.ActivityInitialBinding
 import com.carlosjr.eventsapp.helper.extensions.openActivity
-import com.carlosjr.eventsapp.helper.extensions.toast
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -16,7 +15,6 @@ class InitialActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         onLoadHomeActivity()
-        toast("Initial Activity")
     }
 
     private fun onLoadHomeActivity() {
