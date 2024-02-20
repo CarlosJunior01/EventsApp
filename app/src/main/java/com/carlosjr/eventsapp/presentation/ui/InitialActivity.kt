d509c93be0b6d9a15e7f36aec9c10aa5c0b21cb1
carlos.junior <carlos.udi@live.com>
2024-02-20 00:30:10 -0300

    Updated onLoadHomeActivity
    Added onLoadHomeActivity function for delay time open HomeActivity.

--- app/src/main/java/com/carlosjr/eventsapp/presentation/ui/InitialActivity.kt
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/InitialActivity.kt
@@ -2,7 +2,12 @@ package com.carlosjr.eventsapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.carlosjr.eventsapp.databinding.ActivityInitialBinding
+import com.carlosjr.eventsapp.helper.extensions.openActivity
+import com.carlosjr.eventsapp.helper.extensions.toast
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class InitialActivity : AppCompatActivity() {
 
@@ -10,5 +15,14 @@ class InitialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        onLoadHomeActivity()
+        toast("Initial Activity")
+    }
+
+    private fun onLoadHomeActivity() {
+        lifecycleScope.launch {
+            delay(3500L)
+            openActivity(HomeActivity())
+        }
     }
 }
