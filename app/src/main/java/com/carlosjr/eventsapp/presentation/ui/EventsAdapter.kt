ed2d8059c76605a6b1d6efe75f8e90fe63b9ba7f
carlos.junior <carlos.udi@live.com>
2024-02-21 23:42:03 -0300

    Created EventsAdapter

---
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/EventsAdapter.kt
@@ -0,0 +1,67 @@
+package com.carlosjr.eventsapp.presentation.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.carlosjr.eventsapp.R
+import com.carlosjr.eventsapp.databinding.ItemListEventsBinding
+import com.carlosjr.eventsapp.presentation.model.EventsVO
+import com.squareup.picasso.Picasso
+
+class EventsAdapter(
+    val onClick: (EventsVO) -> Unit
+) : RecyclerView.Adapter<EventsAdapter.EventViewHolder>() {
+
+    //private var movieList: List<Movie> = emptyList()
+    private var eventsList: MutableList<EventsVO> = mutableListOf()
+
+    fun addEventList(eventsList: List<EventsVO>) {
+        //this.movieList = movieList
+        this.eventsList.addAll(eventsList)
+        notifyDataSetChanged()
+    }
+
+    inner class EventViewHolder(eventItem: ItemListEventsBinding) :
+        RecyclerView.ViewHolder(eventItem.root) {
+        private val binding: ItemListEventsBinding
+
+        init {
+            this.binding = eventItem
+        }
+
+        fun bind(events: EventsVO) {
+            val imageUrl = events.image
+
+            //val ratingText = String.format(Locale.US, "%.1f",  movie.voteAverage)
+
+//            val ratingText = BigDecimal(events.voteAverage)
+//                .setScale(1, RoundingMode.HALF_UP).toString()
+
+            binding.textViewEventName.text = events.title
+            binding.textViewEventDay.text = "01"
+            binding.textViewEventMonth.text = "Fev"
+            binding.textViewEventLocation.text = "6 KM"
+            Picasso.get()
+                .load(imageUrl)
+                .error(R.drawable.tech_background)
+                .into(binding.imageEvent)
+
+            binding.cardViewContainer.setOnClickListener {
+                onClick(events)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemBinding = ItemListEventsBinding.inflate(layoutInflater, parent, false)
+        return EventViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        val movie = eventsList[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = eventsList.size
+}
