6e17e4222839cddf29574c6ef391e390477f9de7
carlos.junior <carlos.udi@live.com>
2024-02-21 23:36:48 -0300

    Updated HomeActivity class

--- app/src/main/java/com/carlosjr/eventsapp/presentation/ui/HomeActivity.kt
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/HomeActivity.kt
@@ -1,14 +1,60 @@
 package com.carlosjr.eventsapp.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.carlosjr.eventsapp.databinding.ActivityHomeBinding
+import com.carlosjr.eventsapp.presentation.viewmodel.EventsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+    private val eventsViewModel: EventsViewModel by viewModels()
+    private var eventsAdapter: EventsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupViews()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupAdapter()
+        setupRecyclerView()
+        setupObserve()
+    }
+
+    private fun setupObserve() {
+        eventsViewModel.eventsLiveData.observe(this@HomeActivity) { eventsVO ->
+            eventsAdapter?.addEventList(eventsList = eventsVO)
+        }
+    }
+
+    private fun setupAdapter() {
+        eventsAdapter = EventsAdapter { events ->
+            val intent = Intent(this, DetailsActivity::class.java)
+            intent.putExtra("events", events)
+            startActivity(intent)
+        }
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        recyclerViewEvents.adapter = eventsAdapter
+        recyclerViewEvents.layoutManager = LinearLayoutManager(this@HomeActivity)
+    }
+
+    private fun setupViews() = with(binding) {
+        /*var eventsResult = ""
+        eventsViewModel.eventsLiveData.observe(this@HomeActivity) { userList ->
+            userList.forEach { eventsVO ->
+                eventsResult += "[ID:${eventsVO.id}] - Title: ${eventsVO.title}\n"
+            }
+            textEventsCategory.text = eventsResult
+        }*/
     }
 }
