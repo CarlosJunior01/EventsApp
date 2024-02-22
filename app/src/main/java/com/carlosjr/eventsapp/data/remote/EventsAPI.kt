87b977950a269388c98b34c49f5f4e9bc1999f9d
carlos.junior <carlos.udi@live.com>
2024-02-21 23:45:59 -0300

    Created interface EventsAPI
    Rename EventsDTO model.

--- app/src/main/java/com/carlosjr/eventsapp/data/remote/EventsAPI.kt
+++ app/src/main/java/com/carlosjr/eventsapp/data/remote/EventsAPI.kt
@@ -1,6 +1,6 @@
 package com.carlosjr.eventsapp.data.remote
 
-import com.carlosjr.eventsapp.data.dto.EventsItemDTO
+import com.carlosjr.eventsapp.data.dto.EventsDTO
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -8,10 +8,10 @@ import retrofit2.http.Path
 interface EventsAPI {
 
     @GET("api/events")
-    suspend fun getEvents(): Response<List<EventsItemDTO>>
+    suspend fun getEvents(): Response<List<EventsDTO>>
 
     @GET("api/events{id}")
     suspend fun getEventsById(
         @Path("idPost") id: Int
-    ): Response<EventsItemDTO>
+    ): Response<EventsDTO>
 }
