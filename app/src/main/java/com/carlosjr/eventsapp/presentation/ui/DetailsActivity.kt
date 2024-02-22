6424101b2a01555b7690c447b35769f82b67c2ad
carlos.junior <carlos.udi@live.com>
2024-02-21 23:36:18 -0300

    Created DetailsActivity initial class

---
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.carlosjr.eventsapp.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.carlosjr.eventsapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
