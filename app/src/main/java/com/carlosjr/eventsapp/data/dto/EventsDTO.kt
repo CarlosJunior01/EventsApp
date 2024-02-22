22f775356e716775d537d9020897239dca0465db
carlos.junior <carlos.udi@live.com>
2024-02-21 23:43:38 -0300

    Created EventsDTO model

---
+++ app/src/main/java/com/carlosjr/eventsapp/data/dto/EventsDTO.kt
@@ -0,0 +1,27 @@
+package com.carlosjr.eventsapp.data.dto
+
+import com.carlosjr.eventsapp.domain.bo.EventsBO
+
+data class EventsDTO(
+    val date: Long,
+    val description: String,
+    val id: String,
+    val image: String,
+    val latitude: Double,
+    val longitude: Double,
+    val price: Double,
+    val title: String
+)
+
+fun EventsDTO.toEventsBO(): EventsBO {
+    return EventsBO(
+        date = date,
+        description = description,
+        id = id,
+        image = image,
+        latitude = latitude,
+        longitude = longitude,
+        price = price,
+        title = title
+    )
+}
