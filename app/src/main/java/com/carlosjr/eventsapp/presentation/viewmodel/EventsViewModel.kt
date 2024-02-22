c5416d5b5beda5e3ddee8799e50765e8ac946db8
carlos.junior <carlos.udi@live.com>
2024-02-21 23:46:19 -0300

    Created EventsViewModel

---
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/viewmodel/EventsViewModel.kt
@@ -0,0 +1,29 @@
+package com.carlosjr.eventsapp.presentation.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.carlosjr.eventsapp.domain.usercase.GetEventsUseCase
+import com.carlosjr.eventsapp.presentation.model.EventsVO
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EventsViewModel @Inject constructor(
+    private val useCase: GetEventsUseCase
+): ViewModel() {
+
+    private val _eventsLiveData = MutableLiveData<List<EventsVO>>()
+    val eventsLiveData: MutableLiveData<List<EventsVO>> = _eventsLiveData
+
+    init {
+        getEvents()
+    }
+
+    private fun getEvents() {
+        viewModelScope.launch {
+            _eventsLiveData.postValue(useCase.getEvents())
+        }
+    }
+}
