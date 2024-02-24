06deeb6f6504eea408a452d6a682287d2b00af46
carlos.junior <carlos.udi@live.com>
2024-02-23 21:54:27 -0300

    Updated DetailsActivity
    Implements setupViews to setup actionBarTitle.
    Implements setupListeners primaryActionButton click listener.
    Implements setupResultView to setup the eventsViews.

--- app/src/main/java/com/carlosjr/eventsapp/presentation/ui/DetailsActivity.kt
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/ui/DetailsActivity.kt
@@ -2,7 +2,12 @@ package com.carlosjr.eventsapp.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.carlosjr.eventsapp.R
 import com.carlosjr.eventsapp.databinding.ActivityDetailsBinding
+import com.carlosjr.eventsapp.helper.extensions.parcelable
+import com.carlosjr.eventsapp.presentation.model.EventsVO
+import com.carlosjr.eventsapp.presentation.ui.HomeActivity.Companion.EVENTS_HOME_ACTIVITY_PARAMETERS
+import com.squareup.picasso.Picasso
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -10,5 +15,38 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupViews()
+        setupListeners()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getExtras()
+    }
+
+    private fun getExtras()  {
+        intent.extras?.let {
+            val eventsResult = intent.parcelable<EventsVO>(EVENTS_HOME_ACTIVITY_PARAMETERS)
+            setupResultView(event = eventsResult)
+        }
+    }
+
+    private fun setupViews() = with(binding) {
+        includeActionBar.actionBarTitle.text = getString(R.string.details_event_title)
+    }
+
+    private fun setupListeners() = with(binding) {
+        includeActionBar.primaryActionButton.setOnClickListener { finish() }
+    }
+
+    private fun setupResultView(event: EventsVO?) = with(binding) {
+        includeEventDetail.textViewEventTitle.text = event?.title
+        includeEventDetail.textViewEventDescription.text = event?.description
+        includeEventDetail.textViewEventDate.text = event?.date.toString()
+
+        Picasso.get()
+            .load(event?.image)
+            .error(R.drawable.tech_background)
+            .into(includeEventDetail.imageEvent)
     }
 }
