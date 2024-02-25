129669fbc9eeca87cbbe00ad36139b5ccc0df655
carlos.junior <carlos.udi@live.com>
2024-02-25 20:23:19 -0300

    Updated Build_gradle added new testImplementation and androidTestImplementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,15 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation ("io.mockk:mockk:1.12.4")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("org.mockito:mockito-core:5.10.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
 }
 
 kapt {
