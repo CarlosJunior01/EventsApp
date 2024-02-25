efdaf523aadcb87159b0024f52cb1e25df70c1e5
carlos.junior <carlos.udi@live.com>
2024-02-25 17:06:06 -0300

    Clear code remove unused methods

--- app/src/main/java/com/carlosjr/eventsapp/presentation/model/UiText.kt
+++ app/src/main/java/com/carlosjr/eventsapp/presentation/model/UiText.kt
@@ -1,6 +1,5 @@
 package com.carlosjr.eventsapp.presentation.model
 
-import android.content.Context
 import androidx.annotation.StringRes
 
 sealed class UiText {
@@ -8,21 +7,10 @@ sealed class UiText {
     data class DynamicString(val value: String) : UiText()
     class StringResource(@StringRes val idResource: Int, vararg val args: Any): UiText()
 
-    fun asString(context: Context): String {
-        return when(this) {
-            is DynamicString -> value
-            is StringResource -> context.getString(idResource, *args)
-        }
-    }
-
     companion object {
 
         fun Int.resourceToUiText(): UiText {
             return StringResource(this)
-        }
-
-        fun String.stringToUiText(): UiText {
-            return DynamicString(this)
         }
     }
 }
