040d8a9fde9d58828c159a731f853fba16c3fd45
carlos.junior <carlos.udi@live.com>
2024-02-25 14:44:02 -0300

    Updated EventsRepository added checkInEvent()

--- app/src/main/java/com/carlosjr/eventsapp/data/repository/EventsRepositoryImp.kt
+++ app/src/main/java/com/carlosjr/eventsapp/data/repository/EventsRepositoryImp.kt
@@ -1,9 +1,18 @@
 package com.carlosjr.eventsapp.data.repository
 
+import com.carlosjr.eventsapp.data.dto.CheckInRequest
 import com.carlosjr.eventsapp.data.dto.toEventsBO
+import com.carlosjr.eventsapp.data.remote.CustomNetworkError
 import com.carlosjr.eventsapp.data.remote.EventsAPI
-import com.carlosjr.eventsapp.domain.bo.EventsBO
+import com.carlosjr.eventsapp.domain.model.bo.EventsBO
 import com.carlosjr.eventsapp.domain.repository.EventsRepository
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.Response
 import javax.inject.Inject
 
 class EventsRepositoryImp @Inject constructor(
@@ -21,4 +30,29 @@ class EventsRepositoryImp @Inject constructor(
         }
         return emptyList()
     }
+
+    override fun checkInEvent(checkIn: CheckInRequest): Flow<Void> = flow  {
+        emit(handleApiCall { api.postCheckIn(checkIn) })
+    }.flowOn(Dispatchers.IO)
+
+    private inline fun <reified T> handleApiCall(apiCall: () -> Response<T>): T {
+        val response = apiCall()
+
+        if (response.isSuccessful) {
+            val body = response.body()
+            return if (body is T) body
+            else Gson().fromJson(response.errorBody()?.string(), T::class.java)
+
+        } else {
+            response.errorBody()?.string()?.let {
+                val errorMessage = Gson().fromJson(it, ErrorResponse::class.java)
+                throw CustomNetworkError(errorMessage.message)
+            } ?: throw Throwable("ERROR_ON_PARSE_JSON")
+        }
+    }
+
+    data class ErrorResponse(
+        @SerializedName("message")
+        val message: String? = null
+    )
 }
