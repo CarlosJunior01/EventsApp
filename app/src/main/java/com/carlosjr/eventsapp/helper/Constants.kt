165bdbf8385714ebc7c3eb26a6c6cb4e082607fd
carlos.junior <carlos.udi@live.com>
2024-02-24 23:53:12 -0300

    Updated Constants

--- app/src/main/java/com/carlosjr/eventsapp/helper/Constants.kt
+++ app/src/main/java/com/carlosjr/eventsapp/helper/Constants.kt
@@ -2,4 +2,5 @@ package com.carlosjr.eventsapp.helper
 
 object Constants {
     const val BASE_URL = "https://5f5a8f24d44d640016169133.mockapi.io/"
+    const val SEND_INTENT_TEXT_TYPE = "text/plain"
 }
