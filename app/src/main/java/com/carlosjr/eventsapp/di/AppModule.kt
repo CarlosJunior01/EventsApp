8dbcd081bbf9605b801b4b6c322b3cb7edc81958
carlos.junior <carlos.udi@live.com>
2024-02-24 23:54:50 -0300

    Updated AppModule added provides for CheckInUseCase and provides to DetailsViewModel

--- app/src/main/java/com/carlosjr/eventsapp/di/AppModule.kt
+++ app/src/main/java/com/carlosjr/eventsapp/di/AppModule.kt
@@ -3,16 +3,21 @@ package com.carlosjr.eventsapp.di
 import com.carlosjr.eventsapp.data.remote.EventsAPI
 import com.carlosjr.eventsapp.data.repository.EventsRepositoryImp
 import com.carlosjr.eventsapp.domain.repository.EventsRepository
-import com.carlosjr.eventsapp.domain.usercase.GetEventsUseCase
-import com.carlosjr.eventsapp.domain.usercase.GetEventsUseCaseImp
+import com.carlosjr.eventsapp.domain.usecase.CheckInUseCase
+import com.carlosjr.eventsapp.domain.usecase.GetEventsUseCase
+import com.carlosjr.eventsapp.domain.usecase.GetEventsUseCaseImpl
 import com.carlosjr.eventsapp.helper.Constants
+import com.carlosjr.eventsapp.presentation.viewmodel.DetailsViewModel
 import com.carlosjr.eventsapp.presentation.viewmodel.EventsViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -21,10 +26,20 @@ object AppModule {
 
     @Provides
     @Singleton
+    fun providesOkhttp(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    @Provides
+    @Singleton
     fun providesEventsApi(): EventsAPI {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(providesOkhttp())
             .build()
             .create(EventsAPI::class.java)
     }
@@ -36,11 +51,21 @@ object AppModule {
 
     @Provides
     fun provideEventsUseCase(repository: EventsRepository): GetEventsUseCase {
-        return GetEventsUseCaseImp(repository)
+        return GetEventsUseCaseImpl(repository)
     }
 
     @Provides
-    fun providesUserViewModel(useCase: GetEventsUseCase): EventsViewModel {
+    fun provideCheckInUseCase(repository: EventsRepository): CheckInUseCase {
+        return CheckInUseCase(repository)
+    }
+
+    @Provides
+    fun providesEventsViewModel(useCase: GetEventsUseCase): EventsViewModel {
         return EventsViewModel(useCase)
+    }
+
+    @Provides
+    fun providesDetailsViewModel(useCase: CheckInUseCase): DetailsViewModel {
+        return DetailsViewModel(useCase)
     }
 }
