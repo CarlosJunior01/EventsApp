ac69f2b12a80d4f881062e47cf6fed178b133869
carlos.junior <carlos.udi@live.com>
2024-02-25 14:42:29 -0300

    Updated EventsAPI added suspend fun postCheckIn

--- app/src/main/java/com/carlosjr/eventsapp/data/remote/EventsAPI.kt
+++ app/src/main/java/com/carlosjr/eventsapp/data/remote/EventsAPI.kt
@@ -1,17 +1,17 @@
 package com.carlosjr.eventsapp.data.remote
 
+import com.carlosjr.eventsapp.data.dto.CheckInRequest
 import com.carlosjr.eventsapp.data.dto.EventsDTO
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.POST
 
 interface EventsAPI {
 
     @GET("api/events")
     suspend fun getEvents(): Response<List<EventsDTO>>
 
-    @GET("api/events{id}")
-    suspend fun getEventsById(
-        @Path("idPost") id: Int
-    ): Response<EventsDTO>
+    @POST("api/checkin")
+    suspend fun postCheckIn(@Body checkIn: CheckInRequest) : Response<Void>
 }
