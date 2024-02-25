8128bb79955da6fe22d8f34839126d4d1e2f17c6
carlos.junior <carlos.udi@live.com>
2024-02-24 23:57:39 -0300

    Updated ActivityExt added hideKeyboard extension function

--- app/src/main/java/com/carlosjr/eventsapp/helper/extensions/ActivityExt.kt
+++ app/src/main/java/com/carlosjr/eventsapp/helper/extensions/ActivityExt.kt
@@ -2,11 +2,18 @@ package com.carlosjr.eventsapp.helper.extensions
 
 import android.app.Activity
 import android.content.Intent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 fun Activity.openActivity(activity: Any) = startActivity(Intent(this, activity::class.java))
 
 fun Activity.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+fun Activity.hideKeyboard(view: View) {
+    val inputManager = this.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
 
 
