c93c85ed9833f0b75f8d53db78c20c986af52e91
carlos.junior <carlos.udi@live.com>
2024-02-28 22:45:58 -0300

    Updated StringExt added new convertHttpToHttps function

--- app/src/main/java/com/carlosjr/eventsapp/helper/extensions/StringExt.kt
+++ app/src/main/java/com/carlosjr/eventsapp/helper/extensions/StringExt.kt
@@ -5,8 +5,15 @@ import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.Locale
 
+fun String.convertHttpToHttps() : String {
+    return if (this.contains(HTTPS)) this else this.replace(HTTP, HTTPS)
+}
+
 fun Double.monetaryFormat(): String {
     val decimalFormat: DecimalFormat =
         NumberFormat.getCurrencyInstance(Locale("pt", "BR")) as DecimalFormat
     return decimalFormat.format(BigDecimal(this)).replace("R$", "R$ ")
 }
+
+private const val HTTP = "http"
+private const val HTTPS = "https"
